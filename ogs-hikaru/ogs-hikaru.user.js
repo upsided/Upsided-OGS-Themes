8ce2139a9016b5a8f013d662922cd69e23563e68
upsided <upsided@users.noreply.github.com>
2022-02-18 21:13:14 -0600

    Refactor to allow users to plug in their own  image URLs easily
    Made it much more data-based so users can create their own URL-based themes quickly. Used a more direct way to force-redraw the board. Added the ability to use multiple stone images for that "random" feel
    
    Added comments so users can understand & play with it right away
    
    Also, removed all the "clickCrazy()" stuff,  which made me smile, and cleaned up junk

--- ogs-hikaru/ogs-hikaru.user.js
+++ ogs-hikaru/ogs-hikaru.user.js
@@ -9,105 +9,178 @@
 // ==/UserScript==
 
 (function () {
-    const TIMEOUT = 1000 // time before fake-clicking everything
+    const TIMEOUT = 2000 // time before forced redraw. Set to later if your board doesn't refresh; earlier if the lag bugs you.
+
+    /* edit "theme" variable to your liking.
+
+    Stones are an array of stone image URLS that will be randomly selected to place on the board.
+    Shadows are the same, and correspond to the stone images. If a single shadow is provided for a stone, it will be used for all stones of that color (obvs)
+
+    "Bounds" are the bounding boxes of the stones/shadows, and also correspond to the stone lists. Stones/shadow images will scale to fit these boxes.
+    Bounds are defined by a pair of xy coordinates, where 0,0 is the location of the intersection. [-1,-1] is upper left, [1,1] is bottom right.
+    A box that encompases the whole square is "[-1,-1, 1, 1]"
+
+    You can go further down/right  by about 1.3 with shadows, but not with stones.
+
+    */
+
+    var theme = {
+        "boardImage": "https://github.com/upsided/Upsided-Sabaki-Themes/raw/main/hikaru/board.svg",
+
+        "whiteStones": [
+            "https://raw.githubusercontent.com/upsided/Upsided-OGS-Themes/main/ogs-hikaru/hikaru_white_stone_raw.svg"
+        ],
+
+        "blackStones": [
+            "https://raw.githubusercontent.com/upsided/Upsided-OGS-Themes/main/ogs-hikaru/hikaru_black_stone_raw.svg"
+        ],
+
+        "whiteShadows": [
+            "https://raw.githubusercontent.com/upsided/Upsided-OGS-Themes/main/ogs-hikaru/hikaru_stone_shadow.svg"
+        ],
+
+        "blackShadows": [
+            "https://raw.githubusercontent.com/upsided/Upsided-OGS-Themes/main/ogs-hikaru/hikaru_stone_shadow.svg"
+        ],
+
+        "blackStoneBounds": [ // left, top, right, bottom. This shuffles them up. If you don't like this, remove all but the first one.
+            [-0.97, -0.97, 0.97, 0.97],
+            // [-0.95, -0.95, 0.99, 0.99],
+            // [-1, -1, 0.94, 0.94]
+        ],
+
+        "whiteStoneBounds": [
+            [-0.97, -0.97, 0.97, 0.97],
+            // [-0.95, -0.95, 0.99, 0.99],
+            // [-1, -1, 0.94, 0.94]
+        ],
+
+        "blackShadowBounds": [
+            [-0.85, -0.85, 1, 1.1],
+            // [-0.83, -0.83, 1.02, 1.12],
+            // [-0.88, -0.88, .97, 1.07]
+        ],
+
+        "whiteShadowBounds": [
+            [-0.85, -0.85, 1, 1.1],
+            // [-0.83, -0.83, 1.02, 1.12],
+            // [-0.88, -0.88, .97, 1.07]
+        ]
+
+        // TODO: more board options here like line color, label color, etc.
+    }
 
     function setup() {
 
         console.log("[ogs hikaru] setting up");
 
-        var whitestone = new Image;
-        var blackstone = new Image;
-        var shade = new Image;
+        // images
+        var whitestones = []
+        var blackstones = []
+        var whiteshadows = []
+        var blackshadows = []
 
-        whitestone.src = "https://raw.githubusercontent.com/upsided/Upsided-OGS-Themes/main/ogs-hikaru/hikaru_white_stone_raw.svg"
-        blackstone.src = "https://raw.githubusercontent.com/upsided/Upsided-OGS-Themes/main/ogs-hikaru/hikaru_black_stone_raw.svg"
-        shade.src = "https://raw.githubusercontent.com/upsided/Upsided-OGS-Themes/main/ogs-hikaru/hikaru_stone_shadow.svg"
+        // populate images
+        for (let w of theme.whiteStones) {
+            let i = new Image;
+            i.src = w
+            whitestones.push(i)
+        }
 
-        document.querySelector("[title='Kaya']").style.backgroundImage = "url(\"https://github.com/upsided/Upsided-Sabaki-Themes/raw/main/hikaru/board.svg\")";
+        for (let b of theme.blackStones) {
+            let i = new Image;
+            i.src = b
+            blackstones.push(i)
+        }
 
+        for (let ws of theme.whiteShadows) {
+            let i = new Image;
+            i.src = ws
+            whiteshadows.push(i)
+        }
+
+        for (let bs of theme.blackShadows) {
+            let i = new Image;
+            i.src = bs
+            blackshadows.push(i)
+        }
+
+
+        //setup background image
+        document.querySelector("[title='Kaya']").style.backgroundImage = `url("${theme.boardImage}")`;
+
+        // not sure if needed:
         GoThemes.board.Kaya.prototype.getBackgroundCSS = function () {
             return {
-                "background-image": "url('https://github.com/upsided/Upsided-Sabaki-Themes/raw/main/hikaru/board.svg')",
+                "background-image": `url("${theme.boardImage}")`,
                 "background-color": "#000",
                 "background-size": "cover"
             }
         }
 
+
+        // draw custom white stone
         GoThemes.white.Glass.prototype.placeWhiteStone = function (ctx, shadow_ctx, stone, cx, cy, radius) {
 
-            if (shadow_ctx != undefined)
-                shadow_ctx.drawImage(shade, cx - radius * 0.85, cy - radius * 0.85, radius * 2, radius * 2.1)
+            //random by position
+            let rando = Math.floor(cx*31 + cy*29)
 
-            ctx.drawImage(whitestone, cx - radius * 0.97, cy - radius * 0.97, radius * 1.97, radius * 1.97)
+            if (shadow_ctx != undefined && whiteshadows.length > 0){
+                let img = whiteshadows[0] //whiteshadows[rando%whiteshadows.length]
+                let box = theme.whiteShadowBounds[rando % theme.whiteShadowBounds.length]
+
+                shadow_ctx.drawImage(img, cx + radius * box[0], cy + radius * box[1], radius + radius * box[2], radius + radius * box[3])
+
+            }
+
+            let img = whitestones[rando % whitestones.length]
+            let box = theme.whiteStoneBounds[rando % theme.whiteStoneBounds.length]
+
+            ctx.drawImage(img, cx + radius * box[0], cy + radius * box[1], radius + radius * box[2], radius + radius * box[3])
         };
 
+        //draw custom black stone
         GoThemes.black.Glass.prototype.placeBlackStone = function (ctx, shadow_ctx, stone, cx, cy, radius) {
 
-            if (shadow_ctx != undefined)
-                shadow_ctx.drawImage(shade, cx - radius * 0.85, cy - radius * 0.85, radius * 2, radius * 2.1)
+            //random by position
+            let rando = Math.floor(cx*31 + cy*29)
 
-            ctx.drawImage(blackstone, cx - radius * 0.97, cy - radius * 0.97, radius * 1.97, radius * 1.97)
+            if (shadow_ctx != undefined && blackshadows.length > 0){
+                let img = whiteshadows[0] //whiteshadows[rando%whiteshadows.length]
+                let box = theme.blackShadowBounds[rando % theme.blackShadowBounds.length]
+
+                shadow_ctx.drawImage(img, cx + radius * box[0], cy + radius * box[1], radius + radius * box[2], radius + radius * box[3])
+
+            }
+
+            let img = blackstones[rando % blackstones.length]
+            let box = theme.blackStoneBounds[rando % theme.blackStoneBounds.length]
+
+            ctx.drawImage(img, cx + radius * box[0], cy + radius * box[1], radius + radius * box[2], radius + radius * box[3])
 
         };
 
         console.log("[ogs hikaru] done");
     };
 
-    const mouseClickEvents = ['mousedown', 'click', 'mouseup'];
-    function simulateMouseClick(element) {
-        mouseClickEvents.forEach(mouseEventType =>
-            element.dispatchEvent(
-                new MouseEvent(mouseEventType, {
-                    //view: window,
-                    bubbles: true,
-                    cancelable: true,
-                    buttons: 1
-                })
-            )
-        );
+
+    // touch ogs's preferences object to point to our hack, and to trigger a redraw
+    function triggerRedraw(){
+        preferences.set("goban-theme-board", "Kaya")
+        preferences.set("goban-theme-white", "Glass")
+        preferences.set("goban-theme-black", "Glass")
     }
 
-    function mashCoordinates() {
-        let c = document.getElementsByClassName("ogs-coordinates")
-        if (c.length > 0) {
-            for (let i = 0; i < 6; i++)
-                simulateMouseClick(c[0])
-            //console.log("mashed some coordinates")
-
-        }
-        //setTimeout(mashCoordinates, TIMEOUT) // causes too much lag
-    }
-
-    function clickCrazy() {
-        // have to brute force click stuff to force a redraw
-        // b/c I don't know how to call a board redraw directly
-        let blah = document.querySelectorAll("[title='Plain']")
-        const BRUTE = 5 //
-
-        for (let b = 0; b < BRUTE; b++) {
-            // click the kaya & glass stone settings, because that's where
-            // we placed the theme
-
-            blah = document.querySelectorAll("[title='Kaya']")
-            for (let b of blah) simulateMouseClick(b)
-
-            blah = document.querySelectorAll("[title='Glass']")
-            for (let b of blah) simulateMouseClick(b)
-
-            mashCoordinates() // launches a repeating timer
-
-        }
-
-    }
 
     var setupDone = false
+
     // set up the mutation observer
     // altho this should be installed with @run-at idle, I still saw the code run prior to these globals being available, so just watch the page for updates until they are present
     var observer = new MutationObserver(function (mutations, me) {
         if (typeof data !== "undefined" && typeof GoThemes !== "undefined") {
             if (!setupDone) {
                 setup();
-                setTimeout(clickCrazy, TIMEOUT)
+                setTimeout(triggerRedraw, TIMEOUT)
                 setupDone = true
             }
             me.disconnect(); // stop observing
